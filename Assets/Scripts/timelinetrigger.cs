cc01346faef1960e919179860a315da29a9db69f
Fireboy10008 <mianday100@gmail.com>
2025-11-19 11:48:02 -0400

    I'm not sure what I'm doing

---
+++ Assets/Scripts/timelinetrigger.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class timelinetrigger : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void OnTriggerEnter(Collider other)
+    {
+        // Trigger Timeline
+    }
+}
